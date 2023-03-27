5c1f5b01dd3bd68a805d6e3f4674b0d1273cc194
abhin2002 <abhinajith40@gmail.com>
2023-03-27 11:03:31 +0530

    product api created

--- shopsenseBackEnd/controllers/productController.js
+++ shopsenseBackEnd/controllers/productController.js
@@ -13,6 +13,78 @@ exports.createProduct = async (req,res,next)=>{
     })
 } 
 
-exports.getAllProducts = (req,res) =>{
-    res.status(200).json({message:"Root is working properly"})
+
+//get all products
+exports.getAllProducts = async (req,res) =>{
+
+    const products = await Product.find();
+    res.status(201).json({
+        success:true,
+        products
+    })
+}
+
+
+//get product details
+exports.getProductDetails = async (req,res,next) =>{
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        });
+    }
+
+    res.status(201).json({
+        success:true,
+        products
+    })
+}
+
+
+//Update product --Admin
+exports.updateProduct = async (req,res,next) =>{
+
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    });
+
+    res.status(200).json({
+        success:true,
+        product 
+    })
+}
+
+
+//Delete product --Admin
+
+exports.deleteProduct = async(req,res,next)=>{
+
+    const product = await Product.findByIdAndRemove(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        });
+    }
+
+
+    res.status(200).json({
+        success:true,
+        message:"Product Daleted Successfully"
+    })
 }
