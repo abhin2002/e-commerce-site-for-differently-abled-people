dd5546d6b25c46ed2f61c26b43071f1de4b60b7f
sonarosa <sonarosababloo@gmail.com>
2023-03-29 14:22:07 +0530

    user api connect function

--- shopsenseFrontEnd/src/routes/Signup.js
+++ shopsenseFrontEnd/src/routes/Signup.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React,{useState} from "react";
 import "./Signup.css";
+
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 const Signup=()=>{
+  const navigate = useNavigate();
+  const [user,setUser]=useState({
+    name:"", email:"", password:"", conpassword:""
+  });
 
-  const PostData = async () => {
+  let name,value;
+  const handleInputs=(e)=>{
+    console.log(e);
+    name=e.target.name;
+    value=e.target.value;
+    setUser({...user,[name]:value});
+  }
+  
+  const postdata=async(e)=>{
+     e.preventDefault();
+     const{name,email,password,conpassword}=user;
+      const res = await fetch("/",{method:"POST",
+      headers:{
+      "Content-Type":"/json"
+      },
+      body:JSON.stringify({
+        name,email,password,conpassword
+      })
 
+    });
+    const data =await res.json();
+    if(res.status(422)||!data){
+      window.alert("Invalid registration");
+      console.log("Invalid registration");
+    }else{
+      window.alert("Registration successfull");
+      console.log("Registration successfull");
+      navigate("/login");
+    }
   }
     return(
         <div className="signup-body" style={{background:"linear-gradient(90deg, #ee6352, purple, #ee6352)",
@@ -12,43 +46,56 @@ const Signup=()=>{
            height: "300%",}}>
        <div className="cover">
           <div className="form-body">
-          <form>
+          <form method="POST">
         <h3>Sign Up</h3>
-        <div method="POST" className="mb-3">
+        <div className="mb-3">
           <label>Username</label>
           <input
             type="text"
+            name="name" 
             className="form-control"
+            id="uname"
             placeholder="Username"
-            id = "username"
+            value={user.name}
+            onChange={handleInputs}
           />
         </div>
         <div className="mb-3">
           <label>Email </label>
           <input
             type="email"
+            name="email" 
             className="form-control"
             placeholder="Enter email"
             id="email"
+            value={user.email}
+            onChange={handleInputs}
           />
         </div>
         <div className="mb-3">
           <label>Password</label>
           <input
             type="password"
+            name="password"
             className="form-control"
             placeholder="Enter password"
             id="password"
+            value={user.password}
+            onChange={handleInputs}
           />
         </div>
         <div className="mb-3">
           <label>Confirm password</label>
-          <input type="password" className="form-control" placeholder="Confirm password" id="comfirmPassword" />
+          <input type="password" className="form-control" placeholder="Confirm password"
+          id="cpassword"
+          name="conpassword"
+          value={user.conpassword}
+          onChange={handleInputs} />
         </div>
         <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            SUBMIT 
-          </button> onClick={PostData}
+          <button type="submit" className="btn btn-primary" id="signupbtn" onClick={postdata}>
+            SUBMIT
+          </button>
         </div>
         <p className="forgot-password text-right">
           <Link to="/signin">
